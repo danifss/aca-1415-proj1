b85e4f40d0682374c626dd076e53ea25194859bf
daniel.silva <daniel.silva@ua.pt>
2014-11-15 05:33:56 +0000

    Forwarding para ID/EXE ja da de EXE, mas de MEM e MEM2 nao.

--- forward.cpp
+++ forward.cpp
@@ -18,84 +18,97 @@ void forward::detect_forward()
 
 	printf("Nova iteracao:----------\n");
 
-	
-	if(RegWrite_mem2.read() 
-		&& !RegWrite_exe.read() 
-		&& !RegWrite_mem.read() 
-		&& (WriteReg_mem2.read() == rs_id.read())){ // forwarding MEM2->ID/EXE
-		
-		forwd_mem2_idexe1 = true;
-	 	flag=true;
-	}
-	if(RegWrite_mem.read() 
-		&& !RegWrite_exe.read() 
-		&& (WriteReg_mem.read() == rs_id.read())){ // forwarding MEM->ID/EXE
-		
-		forwd_mem_idexe1 = true;
-		flag=true;
-	}
-	if(RegWrite_exe.read() 
-		&& (WriteReg_exe.read() == rs_id.read())){ // forwarding EXE->ID/EXE
-		
-		forwd_exe_idexe1 = true;
-		flag=true;
-	}
+	if(rd_id.read() != 0){
+		if(RegWrite_mem2.read() 
+			&& !RegWrite_exe.read() 
+			&& !RegWrite_mem.read() 
+			&& (WriteReg_mem2.read() == rs_id.read())){ // forwarding MEM2->ID/EXE
+			
+			forwd_mem2_idexe1 = true;
+		 	flag=true;
+
+		 	forwd_idexe_r1_1.write(0); forwd_idexe_r1_2.write(1);
+		}
+		if(RegWrite_mem.read() 
+			&& !RegWrite_exe.read() 
+			&& (WriteReg_mem.read() == rs_id.read())){ // forwarding MEM->ID/EXE
+			
+			forwd_mem_idexe1 = true;
+			flag=true;
+
+			forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(0);
+		}
+		if(RegWrite_exe.read() 
+			&& (WriteReg_exe.read() == rs_id.read())){ // forwarding EXE->ID/EXE
+			
+			forwd_exe_idexe1 = true;
+			flag=true;
+
+			forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(1);
+		}
 
 
-	if(RegWrite_mem2.read() 
-		&& !RegWrite_exe.read() 
-		&& !RegWrite_mem.read() 
-		&& (WriteReg_mem2.read() == rt_id.read())){ // forwarding MEM2->ID/EXE
-		
-		forwd_mem2_idexe2 = true;
-	 	flag=true;
-	}
-	if(RegWrite_mem.read() 
-		&& !RegWrite_exe.read() 
-		&& (WriteReg_mem.read() == rt_id.read())){ // forwarding MEM->ID/EXE
-		
-		forwd_mem_idexe2 = true;
-		flag=true;
-	}
-	if(RegWrite_exe.read() 
-		&& (WriteReg_exe.read() == rt_id.read())){ // forwarding EXE->ID/EXE
-		
-		forwd_exe_idexe2 = true;
-		flag=true;
+		if(RegWrite_mem2.read() 
+			&& !RegWrite_exe.read() 
+			&& !RegWrite_mem.read() 
+			&& (WriteReg_mem2.read() == rt_id.read())){ // forwarding MEM2->ID/EXE
+			
+			forwd_mem2_idexe2 = true;
+		 	flag=true;
+
+		 	forwd_idexe_r2_1.write(0); forwd_idexe_r2_2.write(1);
+		}
+		if(RegWrite_mem.read() 
+			&& !RegWrite_exe.read() 
+			&& (WriteReg_mem.read() == rt_id.read())){ // forwarding MEM->ID/EXE
+			
+			forwd_mem_idexe2 = true;
+			flag=true;
+
+			forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(0);
+		}
+		if(RegWrite_exe.read() 
+			&& (WriteReg_exe.read() == rt_id.read())){ // forwarding EXE->ID/EXE
+			
+			forwd_exe_idexe2 = true;
+			flag=true;
+
+			forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(1);
+		}
 	}
 
 
 
 	// Gerar sinais de select dos multiplexers
-	if(flag && (rd_id.read() != 0)){
-		//// NAO ALTERAR A ORDEM!!!
-		if(forwd_mem2_idexe1){ // forwd mem2->idexe redata1
-			forwd_idexe_r1_1.write(0); forwd_idexe_r1_2.write(1);
-		}
-		if(forwd_mem2_idexe2){ // forwd mem2->idexe redata2
-			forwd_idexe_r2_1.write(0); forwd_idexe_r2_2.write(1);
-		}
+	// if(flag && (rd_id.read() != 0)){
+	// 	//// NAO ALTERAR A ORDEM!!!
+	// 	if(forwd_mem2_idexe1){ // forwd mem2->idexe redata1
+	// 		forwd_idexe_r1_1.write(0); forwd_idexe_r1_2.write(1);
+	// 	}
+	// 	if(forwd_mem2_idexe2){ // forwd mem2->idexe redata2
+	// 		forwd_idexe_r2_1.write(0); forwd_idexe_r2_2.write(1);
+	// 	}
 
-		if(forwd_mem_idexe1){ // forwd mem->idexe redata1
-			forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(0);
-		}
-		if(forwd_mem_idexe2){ // forwd mem->idexe redata2
-			forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(0);
-		}
+	// 	if(forwd_mem_idexe1){ // forwd mem->idexe redata1
+	// 		forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(0);
+	// 	}
+	// 	if(forwd_mem_idexe2){ // forwd mem->idexe redata2
+	// 		forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(0);
+	// 	}
 		
-		if(forwd_exe_idexe1){ // forwd exe->idexe redata1
-			forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(1);
-		}
-		if(forwd_exe_idexe2){ // forwd exe->idexe redata2
-			forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(1);
-		}
-	}
-	else{ // No forwarding
-		forwd_idexe_r1_1.write(0);
-		forwd_idexe_r1_2.write(0);
-		forwd_idexe_r2_1.write(0);
-		forwd_idexe_r2_2.write(0);
-	}
+	// 	if(forwd_exe_idexe1){ // forwd exe->idexe redata1
+	// 		forwd_idexe_r1_1.write(1); forwd_idexe_r1_2.write(1);
+	// 	}
+	// 	if(forwd_exe_idexe2){ // forwd exe->idexe redata2
+	// 		forwd_idexe_r2_1.write(1); forwd_idexe_r2_2.write(1);
+	// 	}
+	// }
+	// else{ // No forwarding
+	// 	forwd_idexe_r1_1.write(0);
+	// 	forwd_idexe_r1_2.write(0);
+	// 	forwd_idexe_r2_1.write(0);
+	// 	forwd_idexe_r2_2.write(0);
+	// }
 
 	if(!flag)
 		printf("Sem forwarding\n");
@@ -105,6 +118,13 @@ void forward::detect_forward()
 		printf("regdata2 sel0/sel1: %i", forwd_idexe_r2_1.read());
 		printf("%i\n\n", forwd_idexe_r2_2.read());
 	}
+
+	// if(rd_id.read() == 0){
+	// 	forwd_idexe_r1_1.write(0);
+	// 	forwd_idexe_r1_2.write(0);
+	// 	forwd_idexe_r2_1.write(0);
+	// 	forwd_idexe_r2_2.write(0);
+	// }
 
 }
 
