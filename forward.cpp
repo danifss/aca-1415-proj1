2ca75b8666dd814aea4f018684814678bd92dd96
daniel.silva@ua.pt <daniel.silva@ua.pt@a1388dd1-50ee-4ea8-aebf-3645c7f62f4c>
2014-11-17 00:40:54 +0000

    Nem sei que dizer...Mas faz forwards quase todos e tambem mete uns stalls quando necessario.

--- forward.cpp
+++ forward.cpp
@@ -26,43 +26,54 @@ void forward::detect_forward()
 		mux_rt_sel.write(0);
 	}
 
-
-	// mux_forwd_regdata1 ID/EXE 1
-	if (RegWrite_mem.read() && (rs_id.read() == WriteReg_mem.read())){
-		forwd_idexe_r1.write(3);
-	}
-	else if (RegWrite_mem2.read() && (rs_id.read() == WriteReg_mem2.read())){
-		forwd_idexe_r1.write(2);
-	}
-	else if (RegWrite_exe.read() && (rs_id.read() == WriteReg_exe.read())){
-		forwd_idexe_r1.write(1);
-	}
-	else{
+	if(rd_id.read() != 0){
+		// mux_forwd_regdata1 ID/EXE 1
+		if (RegWrite_exe.read() && (rs_id.read() == WriteReg_exe.read())){
+			forwd_idexe_r1.write(1);
+		}
+		else if (RegWrite_mem.read() && (rs_id.read() == WriteReg_mem.read())){
+			forwd_idexe_r1.write(3);
+		}
+		else if (RegWrite_mem2.read() && (rs_id.read() == WriteReg_mem2.read())){
+			forwd_idexe_r1.write(2);
+		}
+		else{
+			forwd_idexe_r1.write(0);
+		}
+		// mux_forwd_regdata2 ID/EXE 2
+		if (RegWrite_exe.read() && (rt_id.read() == WriteReg_exe.read())){
+			forwd_idexe_r2.write(1);
+		}
+		else if (RegWrite_mem.read() && (rt_id.read() == WriteReg_mem.read())){
+			forwd_idexe_r2.write(3);
+		}
+		else if (RegWrite_mem2.read() && (rt_id.read() == WriteReg_mem2.read())){
+			forwd_idexe_r2.write(2);
+		}
+		else{
+			forwd_idexe_r2.write(0);
+		}
+	}else{
 		forwd_idexe_r1.write(0);
-	}
-	// mux_forwd_regdata2 ID/EXE 2
-	if (RegWrite_mem.read() && (rt_id.read() == WriteReg_mem.read())){
-		forwd_idexe_r2.write(3);
-	}
-	else if (RegWrite_mem2.read() && (rt_id.read() == WriteReg_mem2.read())){
-		forwd_idexe_r2.write(2);
-	}
-	else if (RegWrite_exe.read() && (rt_id.read() == WriteReg_exe.read())){
-		forwd_idexe_r2.write(1);
-	}
-	else{
 		forwd_idexe_r2.write(0);
 	}
+	
 
-	// mux_forwd_exemem EXE/MEM
-	if (RegWrite_mem.read() && (rt_exe.read() == WriteReg_mem.read())){
-		forwd_exemem.write(3);
-	}
-	else if (RegWrite_mem2.read() && (rt_exe.read() == WriteReg_mem2.read())){
-		forwd_exemem.write(2);
-	}
-	else if (RegWrite_wb.read() && (rt_exe.read() == WriteReg_wb.read())){
-		forwd_exemem.write(1);
+
+	if(rt_exe.read() != 0){
+		// mux_forwd_exemem EXE/MEM
+		if (RegWrite_mem.read() && (rt_exe.read() == WriteReg_mem.read())){
+			forwd_exemem.write(3);
+		}
+		else if (RegWrite_mem2.read() && (rt_exe.read() == WriteReg_mem2.read())){
+			forwd_exemem.write(2);
+		}
+		else if (RegWrite_wb.read() && (rt_exe.read() == WriteReg_wb.read())){
+			forwd_exemem.write(1);
+		}
+		else{
+			forwd_exemem.write(0);
+		}
 	}
 	else{
 		forwd_exemem.write(0);
